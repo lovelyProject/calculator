38c60e637678d18d4cdc721d2c4d2e30a283c4bb
lovely <edward.hardwork2000@gmail.com>
2022-10-04 16:35:07 +0500

    deleted some lines

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 'use strict'
 let input = document.getElementById('input');
 let list = document.querySelectorAll('.elem');
-let operationList = document.querySelectorAll('.symbol');
 const btn1 = document.getElementById('btn1');
 const btn2 = document.getElementById('btn2');
 const btn3 = document.getElementById('btn3');
@@ -19,18 +18,14 @@ const btnDel = document.getElementById('btnDel');
 const btnUm = document.getElementById('btnUm');
 const clear = document.querySelector('.clear');
 console.log(btn1.textContent)
-function operate(){
-    input.value += String(this.textContent);
-}
+
 function button(){
     input.value += this.textContent;
 }
 for (let elem of list){
     elem.addEventListener('click', button);
 }
-for (let elem of operationList){
-    elem.addEventListener('click', operate);
-}
+
 btnEq.addEventListener('click', function(){
     let res = input.value;
     input.value = eval(res);
